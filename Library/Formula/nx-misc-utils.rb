6a47d5c6724fe99e9b062f4def99ef3fce7d3679
kimura <kimura@abel.local>
2011-05-20 17:22:45 +0900

    Fix nx-misc-utils.

--- Library/Formula/nx-misc-utils.rb
+++ Library/Formula/nx-misc-utils.rb
@@ -3,7 +3,7 @@ require 'formula'
 class NxMiscUtils < Formula
     url 'http://blaise.pinklattice.com/homebrew/nx-misc-utils_0.0.4.tar.gz'
     homepage 'http://blaise.pinklattice.com/homebrew/nx-misc-utils/'
-    md5 '742857c9bd5fa3ec03ed4a41ae09c6e0'
+    md5 '03790ebbc768b55e35427d7db0ccee15'
 
     def install
         system "make PREFIX=#{prefix}"
