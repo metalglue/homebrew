a80122e91ba295bca1675ac1d084dc1226c1df70
Norio Kimura <kimura@lagrange.local>
2011-08-19 17:20:11 +0900

    hardy -> fermat

--- Library/Formula/nx-home-rc.rb
+++ Library/Formula/nx-home-rc.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class NxHomeRc < Formula
-  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.0.38.tar.gz'
+  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.0.39.tar.gz'
   homepage 'http://blaise.pinklattice.com/homebrew/nx-home-rc/'
-  md5 'd8cf3dbd99c092d9d65cdf4df0446c46'
+  md5 '6e63791233eaa16aaf71a8b98ce40083'
 
   def install
     system "make PREFIX=#{prefix}"
