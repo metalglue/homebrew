31d1b8af4585ef5b167cb732aa17508afb2c6079
Norio Kimura <kimura@lagrange.local>
2012-01-14 18:44:40 +0900

    nx-home-rc 0.0.50

--- Library/Formula/nx-home-rc.rb
+++ Library/Formula/nx-home-rc.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class NxHomeRc < Formula
-  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.0.44.tar.gz'
+  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.0.50.tar.gz'
   homepage 'http://blaise.pinklattice.com/homebrew/nx-home-rc/'
-  md5 '58b92f99b42da91e536a4da045e8c46e'
+  md5 '4fe0dbb48f3758e1285251a71ebe65f2'
 
   def install
     system "make PREFIX=#{prefix}"
